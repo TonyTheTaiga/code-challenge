0af97abc965153e636db8fc90de921dcdad2eb7c
tonythetaiga <taigaishida.dev@gmail.com>
2019-08-04 10:18:10 -0700

    re structure

---
+++ cpp/509.fibonacci-number.cpp
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=509 lang=cpp
+ *
+ * [509] Fibonacci Number
+ */
+class Solution {
+public:
+    int fib(int N) {
+        if(N > 1) {return fib(N - 1) + fib(N - 2);}
+        else {return N;}
+    }
+};
+
---
+++ cpp/67.add-binary.cpp
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=67 lang=cpp
+ *
+ * [67] Add Binary
+ */
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        
+        
+    }
+};
+
